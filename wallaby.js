7d86f91df6ee2f4273ffc6389e7aac20912633a2
David Chambers <dlchambersjr@gmail.com>
2018-11-12 14:02:55 -0800

    Inital scaffold

---
+++ wallaby.js
@@ -0,0 +1,36 @@
+module.exports = function (wallaby) {
+
+  return {
+
+    files: ['src/**/*.js', 'tests/supergoose.js'],
+
+    tests: ['tests/**/*.test.js'],
+
+    env: {
+
+      type: 'node',
+
+      runner: 'node',
+
+      params: {
+
+        runner: '--harmony',
+
+      },
+
+    },
+
+    testFramework: 'jest',
+
+    compilers: {
+      '**/*.js': wallaby.compilers.babel(),
+    },
+
+    setup: function () {
+      require('dotenv').config();
+      require('babel-core');
+    },
+
+  };
+
+};
