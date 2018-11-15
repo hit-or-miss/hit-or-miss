3787a1b7b8c66bfc0e000eb2013406b490a04398
George McCadden III <thethirdone.g@gmail.com>
2018-11-14 21:24:24 -0800

    George, Katherine, Ryan: Computer can shoot randomly and it shows on board

--- src/routes/game-route.js
+++ src/routes/game-route.js
@@ -13,54 +13,54 @@ import auth from '../middleware/auth';
 // TODO: Create a route for /play/:fire 
 fireRouter.get('/play/:fire', auth(), async (request, response, next) => {
 
-  let tracker = await Board.findOne({ type: 'tracking', player: request.user._id});
+  let tracker = await Board.findOne({ type: 'tracking', player: request.user._id });
   let computer = await User.findOne({ username: 'Computer' });
   //console.log('COMPUTER', computer);
   // Grab the request.params of :fire
   let coordinates = request.params.fire.toLowerCase();
-  // coordinates;
   
   let tracking3 = await Board.findOne({ type: 'tracking', player: request.user._id });
-  if(tracking3.pastHits.includes(coordinates)){
+  if (tracking3.pastHits.includes(coordinates)){
     response.write('This shot has already been taken, please shoot somewhere else\n');
-  } else {
+  }
+  else {
     tracking3.pastHits.push(coordinates);
-    await Board.findOneAndUpdate({ type: 'tracking', player: request.user._id }, { pastHits: tracking3.pastHits });
+    await Board.findOneAndUpdate({ type: 'tracking', player: request.user._id }, {   pastHits: tracking3.pastHits });
     console.log(tracking3.pastHits);
-
+  
     const row = coordinates[0];
     const col = parseInt(coordinates.slice(1)) - 1;
-
+  
     coordinates = row + col; // this fixes an off by one error in the for loop below
-
-    // if(coordinates.length > 3) return response.send('Please enter a letter and a number for the firing position');
-
+  
+    // if(coordinates.length > 3) return response.send('Please enter a letter and a number   for the firing position');
+  
     console.log(coordinates); // verifies we are grabbing the coordinates for the shot.
     // console.log(request.user); // logs out the user making the request
     // console.log(request.user._id);
     // console.log(request.user._id);
-
+  
     // TODO: Grab all the Ships associated with the User's _id
     let computerShips = await Ship.find({ player: computer._id} );
     console.log(computerShips);
+
   
 
     for(let i = 0; i < computerShips.length; i++){
       if(computerShips[i].location.includes(coordinates)){
         console.log('BEFORE',computerShips[i].location);
-      
+        
         let shipHit = computerShips[i].location.indexOf(coordinates);
         computerShips[i].location.splice(shipHit, 1);
         console.log('AFTER',computerShips[i].location);
         console.log('USER SHIP [i]',computerShips[i]);
-      
-        // If a Ship.location is empty, ie., you've destroyed the whole ship
-        if(computerShips[i].location.length === 0){
+        
+        if (computerShips[i].location.length === 0){
           computerShips[i].sunk = true;
-          await Ship.findOneAndUpdate({ name: computerShips[i].name, player: computer._id }, { sunk: true }, {new: true});
+          await Ship.findOneAndUpdate({name: computerShips[i].name, player: computer._id }, { sunk: true }, { new: true });
         }
-
-        // If all the Ship's locations are empty you've won
+        
+        // if all ships sunk win!!!
         let shipArray = await Ship.find({player: computer._id});
         for (let i = 0; i < shipArray.length; i++) {
           if (shipArray[i].sunk === false) {
@@ -68,38 +68,34 @@ fireRouter.get('/play/:fire', auth(), async (request, response, next) => {
           }
           if (i === shipArray.length - 1 && shipArray[i].sunk === true) {
             response.write('YOU WIN!!!\n');
-            response.end();
           }
         }
 
-
-      
         await Ship.findByIdAndUpdate(computerShips[i]._id, computerShips[i], {new:true});
         console.log('AFTER UPDATE',computerShips[i]);
-      
-        let tracking1 = await Board.findOne({ type: 'tracking', player: request.user._id });  //A5 turn into 'X');
-
+        
+        let tracking1 = await Board.findOne({ type: 'tracking', player: request.user._id });    //A5 turn into 'X');
+  
         tracking1.board[row][col] = 'X';
-
-        tracker = await Board.findOneAndUpdate({ type: 'tracking', player: request.user._id }, { board: tracking1.board }, { new: true });
-
+  
+        tracker = await Board.findOneAndUpdate({ type: 'tracking', player: request.user._id   }, { board: tracking1.board }, { new: true });
+  
         break;
       } else if (i === computerShips.length - 1) {
-        let tracking2 = await Board.findOne({ type: 'tracking', player: request.user._id });  //A5 turn into 'X');
-
+        let tracking2 = await Board.findOne({ type: 'tracking', player: request.user._id });    //A5 turn into 'X');
+  
         tracking2.board[row][col] = 'O';
-
-        tracker = await Board.findOneAndUpdate({ type: 'tracking', player: request.user._id }, { board: tracking2.board }, { new: true });
-
+  
+        tracker = await Board.findOneAndUpdate({ type: 'tracking', player: request.user._id   }, { board: tracking2.board }, { new: true });
+  
         console.log('NO SHIPS MATCH COORDINATES');
         break;
       }
       // console.log(computerShips[i].location);
-
-    }
-  }
   
+    }
 
+  }
   response.write('TRACKING BOARD\n');
   response.write('  1  2  3  4  5  6  7  8  9  10\n');
   response.write('A ' + tracker.board.a.join('  ') + '\n');
@@ -113,6 +109,67 @@ fireRouter.get('/play/:fire', auth(), async (request, response, next) => {
   response.write('I ' + tracker.board.i.join('  ') + '\n');
   response.write('J ' + tracker.board.j.join('  ') + '\n\n');
 
+
+  //SHOOT USER
+  let array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+  let random1 = Math.floor(Math.random() * 10);
+  let random2 = Math.floor(Math.random() * 10);
+  let shotrow = array[random1];
+  let shotcol = random2.toString();
+  let shot = shotrow + shotcol; //Computer shoots user here
+  console.log('SHOT', shot);
+  let userShips = await Ship.find({ player: request.user._id} );
+
+  for(let i = 0; i < userShips.length; i++){
+    if(userShips[i].location.includes(shot)){
+      console.log('BEFORE',userShips[i].location);
+      
+      let shipHit = userShips[i].location.indexOf(shot);
+      userShips[i].location.splice(shipHit, 1);
+      console.log('AFTER',userShips[i].location);
+      console.log('USER SHIP [i]',userShips[i]);
+      
+      if (userShips[i].location.length === 0){
+        userShips[i].sunk = true;
+        await Ship.findOneAndUpdate({name: userShips[i].name, player: request.user._id }, { sunk: true }, { new: true });
+      }
+      
+      // if all your ships sunk lose!!!
+      let shipArray = await Ship.find({player: request.user._id});
+      for (let i = 0; i < shipArray.length; i++) {
+        if (shipArray[i].sunk === false) {
+          break;
+        }
+        if (i === shipArray.length - 1 && shipArray[i].sunk === true) {
+          response.write('YOU LOST!!!\n');
+          response.end();
+        }
+      }
+
+      await Ship.findByIdAndUpdate(userShips[i]._id, userShips[i], {new:true});
+      console.log('AFTER UPDATE',userShips[i]);
+      
+      let primary1 = await Board.findOne({ type: 'primary', player: request.user._id });    //A5 turn into 'X');
+
+      primary1.board[shotrow][shotcol] = 'X';
+
+      tracker = await Board.findOneAndUpdate({ type: 'primary', player: request.user._id   }, { board: primary1.board }, { new: true });
+
+      break;
+    } else if (i === userShips.length - 1) {
+
+      let primary2 = await Board.findOne({ type: 'primary', player: request.user._id });    //A5 turn into 'X');
+
+      primary2.board[shotrow][shotcol] = 'O';
+
+      tracker = await Board.findOneAndUpdate({ type: 'primary', player: request.user._id   }, { board: primary2.board }, { new: true });
+
+      //console.log('NO USER SHIPS MATCH COORDINATES');
+      break;
+    }
+    // console.log(userShips[i].location);
+
+  }
 
   let primary = await Board.findOne({ type: 'primary', player: request.user._id });
 
@@ -136,7 +193,7 @@ fireRouter.get('/play/:fire', auth(), async (request, response, next) => {
   // Ship.find({ player: request.user._id}).then(computerShips => {
   //   console.log(computerShips);
   // });
-  console.log('You finished the GET request');
+  console.log('hello');
   // console.log(computerShips);
   
   
