e6ffcb5d7f27a18a3211bf2c8f573740a3d2c1aa
emeryparks <emeryparks09@hotmail.com>
2018-11-15 08:29:25 -0800

    setup-route testing at 97percent

--- tests/setup-route.test.js
+++ tests/setup-route.test.js
@@ -10,6 +10,7 @@ import supergoose, { startDB, stopDB } from './supergoose.js';
 import User from '../src/models/user-model.js';
 import Board from '../src/models/board-model.js';
 import Ship from '../src/models/ship-model.js';
+import { finished } from 'stream';
 
 const { app } = require('../src/app.js');
 const mockRequest = supergoose(app);
@@ -167,6 +168,138 @@ describe('Setup Routes', () => {
     
       const placeShip = await mockRequest.get('/setup/F-B4-R').auth('foo', 'bar');
       expect(placeShip.status).toBe(404);
+    });
+
+    it('should contain the board text', async () => {
+
+        const userInfo = await User.create( {username: 'foo', password: 'bar',
+      });
+      const shipInfoA = await Ship.create( {name: 'A', size: 5, location:['d1', 'd2', 'd3', 'd4', 'd6'], player: userInfo._id,
+      });
+      const shipInfoB = await Ship.create( {name: 'B', size: 4, location:['d3', 'd4', 'd5', 'd6'], player: userInfo._id,
+      });
+      const shipInfoC = await Ship.create( {name: 'C', size: 3, location:['d3', 'd4', 'd5', 'd6', 'd7'],player: userInfo._id,
+      });
+      const shipInfoD = await Ship.create( {name: 'D',size: 2, location:['d3', 'd4', 'd5', 'd6', 'd7'], player: userInfo._id,
+      });
+      const shipInfoS = await Ship.create( {name: 'S', size: 1, location:['d3', 'd4', 'd5', 'd6', 'd7'], player: userInfo._id,
+      });
+      const boardInfo = await Board.create({type: 'primary', player: userInfo._id,
+        });
+    
+      const placeShip = await mockRequest.get('/setup/A-B4-R').auth('foo', 'bar');
+      expect(placeShip.text).toContain('    1  2  3  4  5  6  7  8  9  10');
+      expect(placeShip.text).toContain('B            A  A  A  A  A');
+    });
+
+    it('should poplulate the board with the ships location when ships direction is right', async () => {
+
+        const userInfo = await User.create( {username: 'foo', password: 'bar',
+      });
+      const shipInfoA = await Ship.create( {name: 'A', size: 5, location:[], player: userInfo._id,
+      });
+      const shipInfoB = await Ship.create( {name: 'B', size: 4, location:[], player: userInfo._id,
+      });
+      const shipInfoC = await Ship.create( {name: 'C', size: 3, location:[],player: userInfo._id,
+      });
+      const shipInfoD = await Ship.create( {name: 'D',size: 2, location:[], player: userInfo._id,
+      });
+      const shipInfoS = await Ship.create( {name: 'S', size: 1, location:[], player: userInfo._id,
+      });
+      const boardInfo = await Board.create({type: 'primary', player: userInfo._id,
+        });
+
+      const placeShip = await mockRequest.get('/setup/A-J1-R').auth('foo', 'bar');
+      const updatedShip = await Ship.findById(shipInfoA._id);
+      expect(updatedShip.location[3]).toBe('j4');
+      });
+
+    it('should poplulate the board with the ships location when ships direction is left', async () => {
+
+      const userInfo = await User.create( {username: 'foo', password: 'bar',
+      });
+      const shipInfoA = await Ship.create( {name: 'A', size: 5, location:[], player: userInfo._id,
+      });
+      const shipInfoB = await Ship.create( {name: 'B', size: 4, location:[], player: userInfo._id,
+      });
+      const shipInfoC = await Ship.create( {name: 'C', size: 3, location:[],player: userInfo._id,
+      });
+      const shipInfoD = await Ship.create( {name: 'D',size: 2, location:[], player: userInfo._id,
+      });
+      const shipInfoS = await Ship.create( {name: 'S', size: 1, location:[], player: userInfo._id,
+      });
+      const boardInfo = await Board.create({type: 'primary', player: userInfo._id,
+        });
+
+      const placeShip = await mockRequest.get('/setup/C-B8-L').auth('foo', 'bar');
+      const updatedShip = await Ship.findById(shipInfoC._id);
+      expect(updatedShip.location[1]).toBe('b7');
+    });
+
+    it('should poplulate the board with the ships location when ships direction is up', async () => {
+
+      const userInfo = await User.create( {username: 'foo', password: 'bar',
+      });
+      const shipInfoA = await Ship.create( {name: 'A', size: 5, location:[], player: userInfo._id,
+      });
+      const shipInfoB = await Ship.create( {name: 'B', size: 4, location:[], player: userInfo._id,
+      });
+      const shipInfoC = await Ship.create( {name: 'C', size: 3, location:[],player: userInfo._id,
+      });
+      const shipInfoD = await Ship.create( {name: 'D',size: 2, location:[], player: userInfo._id,
+      });
+      const shipInfoS = await Ship.create( {name: 'S', size: 1, location:[], player: userInfo._id,
+      });
+      const boardInfo = await Board.create({type: 'primary', player: userInfo._id,
+        });
+
+      const placeShip = await mockRequest.get('/setup/B-J8-U').auth('foo', 'bar');
+      const updatedShip = await Ship.findById(shipInfoB._id);
+      expect(updatedShip.location[1]).toBe('h8');
+    });
+
+    it('should poplulate the board with the ships location when ships direction is down', async () => {
+
+      const userInfo = await User.create( {username: 'foo', password: 'bar',
+      });
+      const shipInfoA = await Ship.create( {name: 'A', size: 5, location:[], player: userInfo._id,
+      });
+      const shipInfoB = await Ship.create( {name: 'B', size: 4, location:[], player: userInfo._id,
+      });
+      const shipInfoC = await Ship.create( {name: 'C', size: 3, location:[],player: userInfo._id,
+      });
+      const shipInfoD = await Ship.create( {name: 'D',size: 2, location:[], player: userInfo._id,
+      });
+      const shipInfoS = await Ship.create( {name: 'S', size: 1, location:[], player: userInfo._id,
+      });
+      const boardInfo = await Board.create({type: 'primary', player: userInfo._id,
+        });
+
+      const placeShip = await mockRequest.get('/setup/D-C1-D').auth('foo', 'bar');
+      const updatedShip = await Ship.findById(shipInfoD._id);
+      expect(updatedShip.location[1]).toBe('d1');
+    });
+
+    it('should poplulate the board with the ships location when ships direction is down', async () => {
+
+      const userInfo = await User.create( {username: 'foo', password: 'bar',
+      });
+      const shipInfoA = await Ship.create( {name: 'A', size: 5, location:[], player: userInfo._id,
+      });
+      const shipInfoB = await Ship.create( {name: 'B', size: 4, location:[], player: userInfo._id,
+      });
+      const shipInfoC = await Ship.create( {name: 'C', size: 3, location:[],player: userInfo._id,
+      });
+      const shipInfoD = await Ship.create( {name: 'D',size: 2, location:[], player: userInfo._id,
+      });
+      const shipInfoS = await Ship.create( {name: 'S', size: 1, location:[], player: userInfo._id,
+      });
+      const boardInfo = await Board.create({type: 'primary', player: userInfo._id,
+        });
+
+      const placeShip = await mockRequest.get('/setup/S-C1-D').auth('foo', 'bar');
+      const updatedShip = await Ship.findById(shipInfoS._id);
+      expect(updatedShip.location[0]).toBe('c1');
     });
   });
 });
